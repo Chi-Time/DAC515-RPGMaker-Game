be8219c2258936b601cffd7cfc756af8b17581f8
James Johnson <music1384@yahoo.co.uk>
2019-03-18 20:38:42 +0000

    Implement cost formula

--- RPG Maker Game/js/plugins/DungeonSystem.js
+++ RPG Maker Game/js/plugins/DungeonSystem.js
@@ -54,6 +54,16 @@ function loadJSONDataFromFile(filename)
         ORANGE: 4
     };
 
+    /** Enum for different in game locations. */
+    var ELocations = {
+         PLANES: 0,
+         FOREST: 1,
+         JUNGLE: 2,
+         VOLCANO: 3,
+         ICE: 4,
+         RUINS: 5
+    };
+
     /** Static class for interacting with the dungeon system. */
     function DungeonSystem ()
     {
@@ -113,11 +123,10 @@ function loadJSONDataFromFile(filename)
     {
         _Game_Interpreter_PlugingCommand.call(this, command, args);
 
-        if (command === "ShowDungeons") 
+        if (command === "ShowDungeons")
         {
             var amount = getRandomIntInRange (1, 4);
             dungeonChoices.length = 0;
-            $gameMessage.clear ();
 
             for (var i = 0; i < amount; i++)
             {
@@ -137,17 +146,8 @@ function loadJSONDataFromFile(filename)
 
             dungeonChoices.push ("Cancel");
             $gameMessage.setChoices (dungeonChoices, 0, dungeonChoices.length - 1);
-            $gameMessage.setChoiceCallback (Awesome.bind (this));
-            //$gameMessage.setChoiceCallback (this.Thing (this, choices));
-
-            // for (var i = 0; i < $gameMessage.choices().length; i++)
-            // {
-            //     // console.log ("cho");
-            //     // console.log ($gameMessage.choices()[i]);
-            //     //NOTE: I think what's happening is that it's binding the last choice with the function only. THIS IS WHATS HAPPENING.
-            //     $gameMessage.setChoiceCallback (this.Thing.bind (this, i, choices));
-            //     console.log (i)
-            // }
+            $gameMessage.add ("hey");
+            $gameMessage.setChoiceCallback (this.testCallback.bind (this));
         }
     };
 
@@ -168,18 +168,20 @@ function loadJSONDataFromFile(filename)
         return false;
     };
 
-    function Awesome (number)
+    //TODO: Document and refactor this method.
+    Game_Interpreter.prototype.testCallback = function (number)
     {
+        var map = null;
         if (number === dungeonChoices.length - 1)
         {
-            console.log ("Cancel: " + number);
+            // console.log ("Cancel: " + number);
             return;
         }
 
         for (var i = 0; i < dungeonMaps.length; i++)
         {
-            console.log ("Running through maps");
-            console.log (number);
+            // console.log ("Running through maps");
+            // console.log (number);
             //console.log (dungeonMaps[i].Map.displayName + " " + dungeonChoices[number]);
             if (dungeonMaps[i].Map.displayName === dungeonChoices[number])
             {
@@ -192,7 +194,10 @@ function loadJSONDataFromFile(filename)
                         //console.log (dungeonMaps[i].Map.events[j]);
                         if (dungeonMaps[i].Map.events[j].note === "Spawn")
                         {
+                            // console.log ("Yeah");
+                            // console.log ("This dungeon costs: " + GetCost (dungeonMaps[i]));
                             $gamePlayer.reserveTransfer(dungeonMaps[i].Map.mapId, dungeonMaps[i].Map.events[j].x, dungeonMaps[i].Map.events[j].y, 0, 0);
+                            
                         }
                         //TODO: Make function which checks if a map has a spawn event inside of it.
                     }
@@ -201,6 +206,16 @@ function loadJSONDataFromFile(filename)
         }
 
         console.log ("Awesome: " + number);
+    };
+
+    /** Calculates the costs of moving to a dungeon.
+     * @param {any} map The map object containing the map info and data to calculate from.
+     * @returns {number} The amount that the trip will cost. */
+    function GetCost (map)
+    {
+        var cost = map.Info.Distance * getRandomFloatInRange (0.1, 0.9) * 100;
+        cost *= $gameParty.members ().length;
+        return Math.round (cost);
     };
 
 })();

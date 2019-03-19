68a0c975d9d586105a92fc5738474d8397c30177
James Johnson <music1384@yahoo.co.uk>
2019-03-19 02:16:01 +0000

    Add custom UI for quest board and player transfer to dungeon.

--- RPG Maker Game/js/plugins/DungeonSystem.js
+++ RPG Maker Game/js/plugins/DungeonSystem.js
@@ -123,6 +123,11 @@ function loadJSONDataFromFile(filename)
     {
         _Game_Interpreter_PlugingCommand.call(this, command, args);
 
+        if (command === "ShowQuests")
+        {
+            SceneManager.push (QuestBoard_Scene);
+        }
+
         if (command === "ShowDungeons")
         {
             var amount = getRandomIntInRange (1, 4);
@@ -148,6 +153,10 @@ function loadJSONDataFromFile(filename)
             $gameMessage.setChoices (dungeonChoices, 0, dungeonChoices.length - 1);
             $gameMessage.add ("hey");
             $gameMessage.setChoiceCallback (this.testCallback.bind (this));
+
+            $gameMessage.onChoice = function () {
+                $gameMessage.add ("Dungeon costs");
+            };
         }
     };
 
@@ -196,7 +205,8 @@ function loadJSONDataFromFile(filename)
                         {
                             // console.log ("Yeah");
                             // console.log ("This dungeon costs: " + GetCost (dungeonMaps[i]));
-                            $gamePlayer.reserveTransfer(dungeonMaps[i].Map.mapId, dungeonMaps[i].Map.events[j].x, dungeonMaps[i].Map.events[j].y, 0, 0);
+                            $gameMessage.clear ();
+                            //$gamePlayer.reserveTransfer(dungeonMaps[i].Map.mapId, dungeonMaps[i].Map.events[j].x, dungeonMaps[i].Map.events[j].y, 0, 0);
                             
                         }
                         //TODO: Make function which checks if a map has a spawn event inside of it.
@@ -216,6 +226,241 @@ function loadJSONDataFromFile(filename)
         var cost = map.Info.Distance * getRandomFloatInRange (0.1, 0.9) * 100;
         cost *= $gameParty.members ().length;
         return Math.round (cost);
+    };
+
+     /** Creates and sets up a craft window for selecting items to craft. */
+     function QuestBoard_QuestWindow () {
+        this.initialize.apply (this, arguments);
+    }
+
+    QuestBoard_QuestWindow.prototype = Object.create(Window_Selectable.prototype);
+    QuestBoard_QuestWindow.prototype.constructor = QuestBoard_QuestWindow;
+
+    QuestBoard_QuestWindow.lastTopRow = 0;
+    QuestBoard_QuestWindow.lastIndex  = 0;
+
+    QuestBoard_QuestWindow.prototype.initialize = function (x, y, height) {
+        this._data = [];
+        this._index = 0;
+        var width = this.windowWidth ();
+        Window_Selectable.prototype.initialize.call (this, x, y, width, height);
+        this._dungeons = [];
+        this.refresh ();
+        this.setTopRow(QuestBoard_QuestWindow.lastTopRow);
+        this.select(QuestBoard_QuestWindow.lastIndex);
+    };
+
+    QuestBoard_QuestWindow.prototype.windowWidth = function () {
+        return 456;
+    };
+
+    QuestBoard_QuestWindow.prototype.item = function () {
+        return this._data[this._index];
+    };
+
+    QuestBoard_QuestWindow.prototype.refresh = function () {
+        this.makeItemList ();
+        this.createContents();
+        this.drawAllItems();
+        this.setCursorFixed (false);
+        this.setCursorAll (false);
+        this.processCursorMove ();
+
+        if (this.cursorFixed ())
+        {
+            console.log ("cursor fixed");
+        }
+        else
+        {
+            console.log ("Cursor is not fixed");
+        }
+
+        if (this.isCursorMovable ())
+        {
+            console.log ("Cursos is movable");
+        }
+        else
+        {
+            console.log ("Cursor is not movabel");
+        }
+    };
+
+    QuestBoard_QuestWindow.prototype.setStatusWindow = function(statusWindow) {
+        this._statusWindow = statusWindow;
+        this.callUpdateHelp();
+    };
+
+    QuestBoard_QuestWindow.prototype.makeItemList = function () 
+    {
+        // Reset the data.
+        this._data.length = 0;
+
+        // Determine how many dungeons to display this time around.
+        var amount = getRandomIntInRange(1, 4);
+
+        for (var i = 0; i < amount; i++) 
+        {
+            // Get a random dungeon map.
+            var map = dungeonMaps[getRandomIntInRange(0, dungeonMaps.length - 1)];
+
+            // If the map is valid.
+            if (map) 
+            {
+                // Determine if the map has been selected before.
+                if (IsSameMap(map.Map.displayName)) 
+                {
+                    // If it has, let's loop again and pick one that hasn't.
+                    i--;
+                    continue;
+                }
+
+                // If it hasn't, then push this map 
+                this._data.push (map);
+            }
+        }
+    };
+
+    QuestBoard_QuestWindow.prototype.drawAllItems = function() {
+        var topIndex = this.topIndex();
+
+        for (var i = 0; i < this._data.length; i++)
+        {
+            var rect = this.itemRectForText(this._index);
+            rect.width -= this.textPadding();
+            this.drawText(this._data[i].Map.displayName, rect.x, i * 36, rect.width, "left");
+        }
+    };
+
+    QuestBoard_QuestWindow.prototype.drawGivenItem = function (index) {
+        var item = this._data[index];
+        var rect = this.itemRect(index);
+        rect.width -= this.textPadding ();
+        this.drawITemName (item, rect.x, rect.y, rect.width - priceWidth);
+    };
+
+    QuestBoard_QuestWindow.prototype.updateHelp = function() {
+        this._helpWindow.setText (this.item ().Info.Description);
+    };
+
+    QuestBoard_QuestWindow.prototype.maxCols = function(){
+		return 1;
+    };
+    
+    //TODO: Figure out why this works at all.
+    QuestBoard_QuestWindow.prototype.maxItems = function(){
+		return this._data ? this._data.length : 0;
+    };
+    
+    // QuestBoard_QuestWindow.prototype.update = function(){
+	// 	if (!$gameMessage.isBusy()){
+	// 		Window_Selectable.prototype.update.call(this);
+	// 		// this.updateStatus();
+	// 		// this.updateIngredients();
+	// 	}
+	// };
+
+    /** Creates and set's up a status window for crafting scenes. */
+    function QuestBoard_StatusWindow () {
+        this.initialize.apply (this, arguments);
+    }
+
+    /** Creates and sets up a status screen for the requirments of each item to craft. */
+    QuestBoard_StatusWindow.prototype = Object.create(Window_Base.prototype);
+    QuestBoard_StatusWindow.prototype.constructor = QuestBoard_StatusWindow;
+
+    QuestBoard_StatusWindow.prototype.initialize = function (x, y, width, height) {
+        Window_Base.prototype.initialize.call (this, x, y, width, height);
+        this._item = null;
+        this._pageIndex = 0;
+        this.refresh ();
+    };
+
+    QuestBoard_StatusWindow.prototype.refresh = function () {
+        this.contents.clear ();
+    };
+
+    /** Creates and displays the quest board scene to the user. */
+    function QuestBoard_Scene () {
+        this.initialize.apply (this, arguments);
+    }
+
+    QuestBoard_Scene.prototype = Object.create (Scene_MenuBase.prototype);
+    QuestBoard_Scene.prototype.constructor = QuestBoard_Scene;
+
+    QuestBoard_Scene.prototype.initialize = function () {
+        Scene_MenuBase.prototype.initialize.call (this);
+    };
+
+    QuestBoard_Scene.prototype.create = function () {
+        Scene_MenuBase.prototype.create.call (this);
+
+        this.createHelpWindow ();
+        this.createQuestWindow ();
+        this.createQuestStatusWindow ();
+        this._questWindow.setStatusWindow (this._statusWindow);
+        this._questWindow.setHandler ('ok',     this.onOk.bind (this));
+        this._questWindow.setHandler ('cancel', this.onCancel.bind(this));
+    };
+
+    //NOTE: USE THIS TO HANDLE ALL SELECTIONS WITHIN THE WINDOW AND DISPLAY RELEVENT THINGS BASED ON THEM.
+    QuestBoard_Scene.prototype.onOk = function ()
+    {
+        var item = this._questWindow.item ();
+
+        if (item) 
+        {
+            for (var i = 0; i < item.Map.events.length; i++) 
+            {
+                if (item.Map.events[i]) 
+                {
+                    if (item.Map.events[i].note === "Spawn") 
+                    {
+                        this.onCancel ();
+                        $gamePlayer.reserveTransfer(item.Map.mapId, item.Map.events[i].x, item.Map.events[i].y, 0, 0);
+                    }
+                    //TODO: Make function which checks if a map has a spawn event inside of it.
+                }
+            }
+        }
+
+        if (item === "Cancel")
+        {
+            this.onCancel ();
+            return;
+        }
+    };
+
+    QuestBoard_Scene.prototype.onCancel = function()
+    {
+        this._questWindow.hide();
+        this._statusWindow.hide();
+        this._helpWindow.clear();
+        this._helpWindow.hide();
+        this.popScene();
+    };
+
+    QuestBoard_Scene.prototype.createHelpWindow = function ()
+    {
+        this._helpWindow = new Window_Help (3);
+        this._helpWindow.y = Graphics.height - this._helpWindow.height;
+        this.addWindow (this._helpWindow);
+    };
+
+    QuestBoard_Scene.prototype.createQuestWindow = function ()
+    {
+        this._questWindow = new QuestBoard_QuestWindow (0, 0, 300);
+        this._questWindow.height = this._helpWindow.y;
+        this._questWindow.setHelpWindow (this._helpWindow);
+        this.addWindow (this._questWindow);
+    };
+
+    QuestBoard_Scene.prototype.createQuestStatusWindow = function ()
+    {
+        this._statusWindow = new QuestBoard_StatusWindow (500, 0, 360, 300);
+        this._statusWindow.x = this._questWindow.windowWidth();
+        this._statusWindow.height = this._helpWindow.y;
+        this.addWindow(this._statusWindow);
+        this._questWindow.activate ();
     };
 
 })();

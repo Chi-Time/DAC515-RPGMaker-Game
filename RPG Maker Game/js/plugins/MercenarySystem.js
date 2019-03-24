b1c8e6ebab47fca47a8f16b312a7378529eba304
James Johnson <music1384@yahoo.co.uk>
2019-03-24 11:25:43 +0000

    Add current merc system state

--- RPG Maker Game/js/plugins/MercenarySystem.js
+++ RPG Maker Game/js/plugins/MercenarySystem.js
@@ -18,6 +18,7 @@
     }
 
     var mercPool = [];
+    var currentMerc = {};
 
     function MercenarySystem ()
     {
@@ -125,27 +126,62 @@
             var eventID = Number (args[0]);
             console.log ($gameMap.event (eventID));
             console.log ($gameMap.event (eventID).event ().Mercenary);
+            currentMerc = $gameMap.event (eventID).event ().Mercenary;
 
-            //TODO: Display mercenary window.
+            //Display mercenary window.
+            SceneManager.push(MercSelection_Scene);
         }
     };
 
-    function Mercenary_MercWindow ()
+    function MercSelection_MercWindow ()
     {
         this.initialize.apply (this, arguments);
     }
 
-    Mercenary_MercWindow.prototype = Object.create (Window_Base.prototype);
-    Mercenary_MercWindow.prototype.constructor = Mercenary_MercWindow;
+    /** Creates and sets up a status screen for the requirments of each item to craft. */
+    MercSelection_MercWindow.prototype = Object.create(Window_Base.prototype);
+    MercSelection_MercWindow.prototype.constructor = MercSelection_MercWindow;
 
-    Mercenary_MercWindow.prototype.initialize = function (x, y, width, height)
+    //TODO: Try and have gold as help window see if that maybe fixes it.
+    MercSelection_MercWindow.prototype.initialize = function (x, y, width, height) {
+        Window_Base.prototype.initialize.call (this, x, y, width, height);
+        this._item = currentMerc;
+        this._pageIndex = 0;
+        this.refresh ();
+    };
+
+    MercSelection_MercWindow.prototype.refresh = function () {
+        console.log ("merc window called");
+        //this.contents.clear ();
+        //TODO: Format and display information here.
+        this.drawText ($gameActors.actor (currentMerc.Actor.id).actor ().name, 0, 0, 300, "left");
+        this.drawText ($gameActors.actor (currentMerc.Actor.id).currentClass().name, 300, 0, 300, "left");
+    };
+
+    MercSelection_MercWindow.prototype.setItem = function(item) {
+        if (this._item !== item) {
+            this._item = item;
+            this.refresh();
+        }
+    };
+
+    function MercSelection_GoldWindow ()
     {
+        this.initialize.apply (this, arguments);
+    }
+
+    MercSelection_GoldWindow.prototype = Object.create (Window_Base.prototype);
+    MercSelection_GoldWindow.prototype.constructor = MercSelection_GoldWindow;
+
+    MercSelection_GoldWindow.prototype.initialize = function (x, y, width, height)
+    {
+        console.log ("gold constructor")
         Window_Base.prototype.initialize.call (this, x, y, width, height);
         this._item = null;
         this.refresh ();
     };
 
-    Mercenary_MercWindow.prototype.setItem = function(item) 
+    MercSelection_GoldWindow.prototype.setItem = function(item) 
     {
         if (this._item !== item) {
             this._item = item;
@@ -153,9 +189,91 @@
         }
     };
 
-    Mercenary_MercWindow.prototype.refresh = function ()
+    MercSelection_GoldWindow.prototype.refresh = function ()
     {
-        //TODO: Format and display information here.
+        console.log ("Called");
+        this.contents.clear ();
+        this.drawText ("Gold: " + $gameParty.gold (), 0, 0, 250, "left");
+        this.drawText ("Cost: " + 400, 0, 36, 250, "left");
+    };
+
+    function MercSelection_HireWindow ()
+    {
+        this.initialize.apply (this, arguments);
+    }
+
+    MercSelection_HireWindow.prototype= Object.create (Window_HorzCommand.prototype);
+    MercSelection_HireWindow.prototype.constructor = MercSelection_HireWindow;
+
+    MercSelection_MercWindow.prototype.initialize = function (x, y, width, height)
+    {
+        Window_Selectable.prototype.initialize.call (this, x, y, width, height);
+    };
+
+    MercSelection_HireWindow.prototype.windowWidth = function () {
+        return 456;
+    };
+
+    function MercSelection_Scene ()
+    {
+        this.initialize.apply (this, arguments);
+    }
+
+    MercSelection_Scene.prototype = Object.create (Scene_MenuBase.prototype);
+    MercSelection_Scene.prototype.constructor = MercSelection_Scene;
+
+    MercSelection_Scene.prototype.initialize = function () {
+        Scene_MenuBase.prototype.initialize.call (this);
+    };
+
+    MercSelection_Scene.prototype.create = function ()
+    {
+        Scene_MenuBase.prototype.create.call (this);
+        this.createGoldWindow ();
+        this.createHireWindow ();
+        this.createMercWindow ();
+        this._mercWindow.refresh ();
+        this._hireWindow.activate ();
+
+        this._hireWindow.setHandler ("ok", this.onOk.bind (this));
+        this._hireWindow.setHandler ("cancel", this.onCancel.bind(this));
+    };
+
+    MercSelection_Scene.prototype.createGoldWindow = function ()
+    {
+        this._goldWindow = new MercSelection_GoldWindow (0, 0, 450, 100);
+        this._goldWindow.y = Graphics.height - this._goldWindow.height;
+        this.addWindow (this._goldWindow);
+    };
+
+    MercSelection_Scene.prototype.createHireWindow = function ()
+    {
+        this._hireWindow = new MercSelection_HireWindow (0, 0, 0, 100);
+        this._hireWindow.x = this._goldWindow.width;
+        this._hireWindow.width = Graphics.width - this._hireWindow.x;
+        this._hireWindow.y = Graphics.height - this._hireWindow.height;
+        this.addWindow (this._hireWindow);
+    };
+
+    MercSelection_Scene.prototype.createMercWindow = function ()
+    {
+        console.log ("drawing merc window");
+        this._mercWindow = new MercSelection_MercWindow (0, 0, 0, 0);
+        this._mercWindow.width = Graphics.width;
+        this._mercWindow.height = this._goldWindow.y;
+        this.addWindow (this._mercWindow);
+    };
+
+    MercSelection_Scene.prototype.onOk = function ()
+    {
+    };
+
+    MercSelection_Scene.prototype.onCancel = function ()
+    {
+        this._goldWindow.hide ();
+        this._hireWindow.hide ();
+        this._mercWindow.hide ();
+        this.popScene();
     };
 
 })();
